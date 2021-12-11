99d38ea60bff119f855fa3651a7816a29dda17f1
Vasundhara Agarwal <vasundhara6999@gmail.com>
2021-12-12 01:45:54 +0530

    hardcoded some parameters for ease of interpretation

--- models.py
+++ models.py
@@ -20,20 +20,20 @@ class OutputLayer(nn.Module):
         return self.l(x)
         
 class CIFAR10Encoder(nn.Module):
-    def __init__(self, input_dim, hidden_dims) -> None:
+    def __init__(self) -> None:
         super().__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
         
-        self.fc1 = nn.Linear(input_dim, hidden_dims[0])
-        self.fc2 = nn.Linear(hidden_dims[0], hidden_dims[1])
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
 
     def forward(self, x):
         x = self.pool(self.relu(self.conv1(x)))
         x = self.pool(self.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 4 * 4)
+        x = x.view(-1, 16 * 5 * 5)
 
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
